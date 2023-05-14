02beea78b03418065084a43dbff9c4932c48444e
maxgda <maxglaida@gmail.com>
2023-05-14 13:05:33 +0200

    refactored Artist and Track class for ES01

--- MusicLandscape/src/MusicLandscape/entities/Artist.java
+++ MusicLandscape/src/MusicLandscape/entities/Artist.java
@@ -4,17 +4,20 @@ public class Artist {
     private String name;
 
     public String getName() {
-        // gets the name of this artist
         return name;
     }
     public void setName(String name) {
-        // sets the name of this artist.
-        // the name of an artist cannot be null or empty.
-        // if an invalid argument is passed to the method the state of the object remains unchanged
-        if ( name != null  && name.trim().length() > 0 )
-        // possibility from the guidance video is:    if ( name != null && !name.isEmpty() ) {...}
-        // !! this however does not pass the "  " test !!
-            this.name=name;
+        if (argumentIsValid(name)) {
+            this.name = name;
+        } else {
+            // its good to have some kind of feedback ( logging )
+            System.out.printf("Invalid name argument: %s%n", name);
+        }
+    }
+
+    // not present in the class diagramm but a nice way to make the code more readable.
+    private boolean argumentIsValid(String name) {
+        return ( name != null  && name.trim().length() > 0 );
     }
 
 }
--- MusicLandscape/src/MusicLandscape/entities/Track.java
+++ MusicLandscape/src/MusicLandscape/entities/Track.java
@@ -1,117 +1,99 @@
 package MusicLandscape.entities;
 
 public class Track {
-    private String title = null;        // the title of this track. a null title represents an unknown title
-                                        // I THINK THAT strings are Null-filled by default, but it's set NULL by default
-    private int duration = 0;           // the duration of this track in seconds the duration is a non-negative number
-                                        // duration 0 (zero) represents unknown duration = initial value
-    private Artist writer = new Artist();   // the artist who wrote this track the writer cannot be null
-    private Artist performer = new Artist();// the artist who performs this track the performer cannot be null
-    private int year = 0;               // the year in which the Track was or will be produced valid years are between 1900- 2999
-    // initial value = 0
-
+    private String title     = null;
+    private int    duration  = 0;
+    private Artist writer    = new Artist();
+    private Artist performer = new Artist();
+    private int    year      = 0;
 
     public int getYear() {
-        // gets the production year of this track
         return year;
     }
+
     public void setYear(int year) {
-        // sets the production year of this track valid years are between 1900 and 2999
-        // other values are ignored, the object remains unchanged
-        if( year>=1900 && year<=2999)
+        if (year >= 1900 && year <= 2999) {
             this.year = year;
+        } else {
+            System.out.printf("year is out of range (year >= 1900 && year <= 2999), year: %s%n", year);
+        }
     }
+
     public int getDuration() {
-        // gets the duration of this track
         return duration;
     }
 
     public void setDuration(int inputDuration) {
-        // sets the duration a negative value is ignored, the object remains unchanged
-        if( inputDuration>=0 )
+        if (inputDuration >= 0) {
             this.duration = inputDuration;
+        } else {
+            System.out.printf("duration cant be negative, duration: %d", inputDuration);
+        }
     }
+
     public String getTitle() {
-        // gets the title of this track.
-        //  if the title is not known (null) "unknown title" is returned (without quotes)
-        if ( title == null )
+        if (title == null) {
             return "unknown title";
+        }
         return title;
     }
 
     public void setTitle(String title) {
-        // sets the title of this track.
         this.title = title;
     }
 
     public Artist getWriter() {
-        // returns the writer of this track
         return writer;
     }
 
     public void setWriter(Artist writer) {
-        // sets the writer of this track null arguments are ignored
-        if( writer != null )
+        if (writer != null) {
             this.writer = writer;
+        } else {
+            System.out.println("setting a null writer is invalid");
+        }
     }
+
     public Artist getPerformer() {
-        // returns the performer of this track
         return performer;
     }
 
     public void setPerformer(Artist performer) {
-        // sets the performer of this track null arguments are ignored
-        if( performer != null )
+        if (performer != null) {
             this.performer = performer;
+        } else {
+            System.out.println("setting a null performer is invalid");
+        }
     }
 
     public boolean writerIsKnown() {
-        // this getter is used to check if the writer of this Track is known.
-        // A writer is considered as known if the name is not equal to null.
-        if( this.writer == null )
-           return false;
+        if (this.writer == null) {
+            return false;
+        }
         return writer.getName() != null;
     }
 
     public String getString() {
-        // returns a formatted String containing all information of this track.
-        // the String representation is (without quotes):
-        //          "title by writer performed by performer (min:sec)"
-        // where:
-        // title    stands for the title (exactly 10 chars wide) if not set, return unknown
-        // writer   stands for the writer name (exactly 10 chars wide, right justified)
-        // performer stands for the performer name (exactly 10 chars wide, right justified)
-        // min      is the duration's amount of full minutes (at least two digits, leading zeros)
-        // sec      is the duration's remaining amount of seconds (at least two digits, leading zeros)
+        return String.format("%10.10s by %10.10s performed by %10.10s (%02d:%02d)",
+                formatTitle(),
+                formatArtistName(writer),
+                formatArtistName(performer),
+                formatDurationToMin(),
+                formatDurationToSec());
+    }
 
-        // The text above is given from the java doc.
-        // !!! HOWEVER, the test routines also demand "unknown" outputs for empty writer and performer !!!
-        // Therefore, i create some helping variables, check the content and prepare the output data:
-
-        String checkedTitle, checkedWriter, checkedPerformer;
-
-        checkedTitle = title == null ? "unknown" : title;
-        // i wrote the following version, but IntelliJ forced me to change it into one of three given options.
-        // i chose number three: See above.
-        // This was mine:
-        //         if ( title == null )
-        //            checkedTitle = "unknown";
-        //        else
-        //            checkedTitle = title;
-        //
-        // The IntelliJ-recommended version (which i dont understand without reading further) is:
-        // checkedTitle = Objects.requireNonNullElse(title, "unknown");
-
-        if ( writer == null || writer.getName() == null )
-            checkedWriter = "unknown";
-        else
-            checkedWriter = writer.getName();
-
-        if ( performer == null || performer.getName() == null )
-            checkedPerformer = "unknown";
-        else
-            checkedPerformer = performer.getName();
-        
-        return String.format("%10.10s by %10.10s performed by %10.10s (%02d:%02d)", checkedTitle, checkedWriter, checkedPerformer, duration/60, duration%60);
+    // not present in the class diagramm but a nice way to make the code more readable.
+    // you can make as many private methods as you like to make the code more readable.
+    private int formatDurationToMin() {
+        return this.duration / 60;
+    }
+    private int formatDurationToSec() {
+        return this.duration % 60;
+    }
+    private String formatTitle() {
+        return title == null ? "unknown" : title;
+    }
+    private String formatArtistName(Artist artist) {
+        return (artist == null || artist.getName() == null) ? "unknown" : artist.getName();
     }
 }
